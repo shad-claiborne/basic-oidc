03c46eb2fd3705f02a8246ac20ea8e35efa34070
Shad Claiborne <shad.claiborne@outlook.com>
2026-01-16 14:42:29 -0800

    removed middleware skeleton

--- tests/middleware/hono/index.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, expect, test } from 'vitest';
-
-describe("say what", () => {
-    test('hello', () => {
-        const input = Math.sqrt(4);
-        expect(input).to.equal(2);
-    });
-});
